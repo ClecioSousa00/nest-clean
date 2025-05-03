34d657e4046041bf3c130bcb34f30872be7edc1a
ClecioSousa000 <cleciosouza015@gmail.com>
2025-05-03 10:06:19 -0300

    Feat: aula 216 - controller de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+        console.log(Buffer.from(privateKey, 'base64').toString('utf-8'))
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      },
+    }),
+  ],
+})
 export class AuthModule {}
