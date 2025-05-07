7dafd911684b90e2119b4d27993fb0b7a42c6891
ClecioSousa000 <cleciosouza015@gmail.com>
2025-05-07 20:03:17 -0300

    Feat: aula 239 testes de cadastro de autenticação

---
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { AuthenticateStudentUseCase } from './authenticate-student'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { makeStudent } from 'test/factories/make-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+
+let authenticateStudentUseCase: AuthenticateStudentUseCase
+
+describe('Authenticate Student Use Case', () => {
+  beforeEach(() => {
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+
+    authenticateStudentUseCase = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate student', async () => {
+    const student = makeStudent({
+      email: 'john@gmail.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.items.push(student)
+
+    const result = await authenticateStudentUseCase.execute({
+      email: 'john@gmail.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
---
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -0,0 +1,45 @@
+import { RegisterStudentUseCase } from './register-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+
+let registerStudentUseCase: RegisterStudentUseCase
+
+describe('Register Student Use Case', () => {
+  beforeEach(() => {
+    fakeHasher = new FakeHasher()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    registerStudentUseCase = new RegisterStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+    )
+  })
+
+  it('should be able to register a new student', async () => {
+    const result = await registerStudentUseCase.execute({
+      name: 'john',
+      email: 'john@gmail.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0],
+    })
+  })
+
+  it('should hash student password upon registration', async () => {
+    const result = await registerStudentUseCase.execute({
+      name: 'john',
+      email: 'john@gmail.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+})
