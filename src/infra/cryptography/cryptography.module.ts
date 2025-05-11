29ac285c388e79e4f73b341b940281bd389c71f0
ClecioSousa000 <cleciosouza015@gmail.com>
2025-05-11 10:07:06 -0300

    Feat: aula 241 - refatorando controller de autenticação

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -13,4 +13,4 @@ import { HashGenerator } from '@/domain/forum/application/cryptography/hash-gene
   ],
   exports: [Encrypter, HashCompare, HashGenerator],
 })
-export class Cryptography {}
+export class CryptographyModule {}
