dbed79e27aa4ec50b5ff3ca38267a66a26244021
ClecioSousa00 <cleciosouza015@gmail.com>
2025-05-21 09:28:27 -0300

    Feat: aula 295 - value object detalhes da pergunta

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,14 +1,19 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
-import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
+import { InMemoryAttachmentsRepository } from './in-memory-attachments-repository'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { InMemoryQuestionAttachmentRepository } from './in-memory-question-attachment-repository'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   items: Question[] = []
 
   constructor(
-    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+    private questionAttachmentsRepository: InMemoryQuestionAttachmentRepository,
+    private attachmentsRepository: InMemoryAttachmentsRepository,
+    private studentsRepository: InMemoryStudentsRepository,
   ) {}
 
   async create(question: Question) {
@@ -22,6 +27,52 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     if (!slugQuestion) return null
 
     return slugQuestion
+  }
+
+  async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
+    const slugQuestion = this.items.find((item) => item.slug.value === slug)
+
+    if (!slugQuestion) return null
+
+    const author = this.studentsRepository.items.find((student) =>
+      student.id.equals(slugQuestion.authorId),
+    )
+
+    if (!author) {
+      throw new Error(
+        `Author with ID "${slugQuestion.authorId.toString()}" does not exist`,
+      )
+    }
+
+    const questionAttachments = this.questionAttachmentsRepository.items.filter(
+      (questionAttachment) => questionAttachment.id.equals(slugQuestion.id),
+    )
+
+    const attachments = questionAttachments.map((questionAttachment) => {
+      const attachment = this.attachmentsRepository.items.find((attachment) =>
+        attachment.id.equals(questionAttachment.id),
+      )
+
+      if (!attachment) {
+        throw new Error(
+          `Attachment with ID "${questionAttachment.id.toString()}" does not exist`,
+        )
+      }
+      return attachment
+    })
+
+    return QuestionDetails.create({
+      questionId: slugQuestion.id,
+      authorId: slugQuestion.authorId,
+      author: author.name,
+      title: slugQuestion.title,
+      slug: slugQuestion.slug,
+      content: slugQuestion.content,
+      bestAnswerId: slugQuestion.bestAnswerId,
+      attachments,
+      createdAt: slugQuestion.createdAt,
+      updatedAt: slugQuestion.updatedAt,
+    })
   }
 
   async delete(question: Question) {
